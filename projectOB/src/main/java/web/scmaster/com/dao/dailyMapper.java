7919cf644af7c9e92fe15ce2453940d0fb0e0234
kita <kita@kita-PC>
2017-03-29 16:15:55 +0900

    2017-03-29 16:15 박재만 운동select

--- projectOB/src/main/java/web/scmaster/com/dao/dailyMapper.java
+++ projectOB/src/main/java/web/scmaster/com/dao/dailyMapper.java
@@ -1,13 +1,11 @@
 package web.scmaster.com.dao;
 
 import java.util.HashMap;
-
-import web.scmaster.com.vo.Exercise;
+import java.util.List;
 import web.scmaster.com.vo.Meal;
 
 public interface dailyMapper {
 	
 	public Meal searchMeal(HashMap<String, Object> map);
-	public Exercise searchExercise(HashMap<String, Object> map);
-	
+	public List<HashMap<String, Object>> exerciseList(HashMap<String, Object> map);
 }
