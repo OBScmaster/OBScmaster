a1c7f8fdf07df52f67c0a34bc2a85a94f5156061
kita <kita@kita-PC>
2017-03-29 15:19:31 +0900

    ㅁ

--- projectOB/src/main/java/web/scmaster/com/dao/dailyMapper.java
+++ projectOB/src/main/java/web/scmaster/com/dao/dailyMapper.java
@@ -2,9 +2,6 @@ package web.scmaster.com.dao;
 
 import java.util.HashMap;
 
-import org.apache.ibatis.annotations.Param;
-
-import web.scmaster.com.vo.Daily2;
 import web.scmaster.com.vo.Exercise;
 import web.scmaster.com.vo.Meal;
 
